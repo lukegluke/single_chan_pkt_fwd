c48ebd340378562c474f430b5340a714765ff696
Oleg <ev.mipt@gmail.com>
2017-08-07 22:50:04 +0300

    test configuration

--- main.cpp
+++ main.cpp
@@ -91,7 +91,7 @@ static char description[64] = "";                        /* used for free form d
 
 // define servers
 // TODO: use host names and dns
-#define SERVER1 "54.72.145.119"    // The Things Network: croft.thethings.girovito.nl
+#define SERVER1 "52.169.76.203"    // The Things Network: router.eu.thethings.network
 #define SERVER2 "192.168.1.135"    // local
 #define PORT 1700                  // The port on which to send data
 
